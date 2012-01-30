3076ed221615bbf4c7d46ff54e64118d8c9918cd
yiminghe <yiminghe@gmail.com>
2012-01-30 18:09:38 +0800

    support kissy > 1.2

--- example/overlay.js
+++ example/overlay.js
@@ -1,33 +1,45 @@
+var path = require("path"),
+    fs = require("fs");
 
-var S = require('../lib/kissy').KISSY;
+require("kissy-nodejs.js");
 
-var http = require('http'),
-	fs = require('fs');
+var S = KISSY;
+console.log(S.isNumber({}));
+console.log(S.isNumber(1));
+//console.log(S);
 
-S.use('overlay',function(S) {
+S.use("overlay", function(S, Overlay) {
+    document.body.innerHTML = "<div id='server'></div>";
+    var d = new Overlay.Dialog({
+        width:"500px",
+        render:"#server",
+        elCls:'server-dialog',
+        headerContent:"服务器端 header",
+        bodyContent:"服务器端 body",
+        footerContent:"服务器端 footer"
+    });
+    d.center();
+    d.show();
+    console.log(d.get("el")[0].offsetWidth);
+    console.log(d.get("el")[0].offsetHeight);
+    console.log(d.get("el").offset());
+    console.log(d.get("x"));
 
-	new S.Dialog({
-		content: '再怎么滚, 我都不会动~',
-		fixed: true
-	}).show();
-	var dialog = new S.Dialog({
-		containerCls: 'dialog4',
-		width: 400,
-		height: 300,
-		header: 'this is title',
-		footer: 'footer',
-		content: 'content',
-		mask: false,
-		align: {
-			node: '.constrain',
-			points: ['bc', 'cc']
-		},
-		constrain: '.constrain',
-		draggable: true
-	});
-	dialog.show();
-	new S.Mask();
-	S.log(document.outerHTML);
+    var DOM = S.DOM,offset = { left: DOM.scrollLeft(), top: DOM.scrollTop() },
+        w = DOM['viewportWidth'](),
+        h = DOM['viewportHeight']();
 
+    console.log(offset, w, h);
 
+    document.getElementsByTagName("head")[0].innerHTML = "<meta charset='utf-8'/>" +
+        "<link rel='stylesheet' " +
+        "href='http://docs.kissyui.com/kissy/src/overlay/assets/cool.css'/>" +
+        "<style>" +
+        "#server .server-dialog {" +
+        "left:100px;" +
+        "top:100px;" +
+        "}" +
+        "</style>";
+    console.log(document.innerHTML);
+    fs.writeFile("./tools/nodejs-patch/overlay.html", document.innerHTML);
 });
